508e4003b3acddf9d30dfcb69ff4bd07a81d7965
ValerioL96 <v.lembo1396@gmail.com>
2024-05-14 17:36:11 +0200

    Added in javascript fruit list with array

---
+++ array-lista/js/script.js
@@ -0,0 +1,19 @@
+//1.creare l'array con la frutta del frigorifero:
+    //- Mi creo una lista della frutta che si trova all'interno del frigo
+
+     const fruitList = [
+        'banana',
+        'mela',
+        'pera',
+        'ciliegia',
+        'arancia',   
+        'mandarino',
+        'cocomero',
+        'limone',
+        'fragola'
+        ];
+
+    //- stampo in console per verificare la mia lista ;
+
+    console.log(fruitList);
+    
