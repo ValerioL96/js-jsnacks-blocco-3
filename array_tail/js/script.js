6c9b13dd63dc336470dc2d01a54a9e24f246a49b
ValerioL96 <v.lembo1396@gmail.com>
2024-05-15 15:31:11 +0200

    cretaded newarray with you cicle for

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -9,3 +9,16 @@
   if(userNumberEl> 20 || userNumberEl<1 || Number.isNaN(userNumberEl)){
     userNumberEl = 12;
   }
+
+  //2) Creo un array
+
+  const newArray = [];
+
+  //-Creo un ciclo per quanto sono gli elementi casuali dell'utente 
+
+  for(let i= 0 ; i < userNumberEl ; i++){
+     newArray.push(Math.floor(Math.random()*100)+1);
+    } 
+
+    
+
