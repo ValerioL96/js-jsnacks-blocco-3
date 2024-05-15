0ff046faebb7406611b827eb7dcb7257193d2245
ValerioL96 <v.lembo1396@gmail.com>
2024-05-15 12:29:38 +0200

    Added snack-10

---
+++ Snack-10/js/script.js
@@ -0,0 +1,20 @@
+//snack-10
+//Crea due array che hanno un numero di elementi diversi.
+
+const arrayNumberA = [1,2,3,4,5,6,7,];
+const arrayNumberB = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+let arrayLenghtA = arrayNumberA.length;
+let arrayLenghtB = arrayNumberB.length;
+
+//Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro
+
+const newElement = [1];
+
+while(arrayLenghtA ==arrayLenghtB === true){
+    console.log(newElement);
+
+    newElement++
+
+
+}
