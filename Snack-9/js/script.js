9ca22cb3d1062d962fc8513d9ffe4cc89766b783
ValerioL96 <v.lembo1396@gmail.com>
2024-05-15 11:58:20 +0200

    Added snack-9

---
+++ Snack-9/js/script.js
@@ -0,0 +1,12 @@
+//Snack9 Crea un array vuoto. 
+
+const array = [];
+
+//Chiedi fino a che l'array non ha 6 numeri all'utente di inserire un numero,
+ //se è dispari inseriscilo nell'array.
+
+ let  number = Number.parseInt(prompt('Type number'), 10);
+
+ while(number%2 ==1){
+    number = Number.parseInt(prompt('Type number'), 10);
+ }
