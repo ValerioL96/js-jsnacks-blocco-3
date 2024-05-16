db29dcfb2e2c0fa2321e461e15bd1c83bad82e88
ValerioL96 <v.lembo1396@gmail.com>
2024-05-16 12:02:09 +0200

    Correction snak-12

--- Snack-12/js/script.js
+++ Snack-12/js/script.js
@@ -1,24 +1,30 @@
 // Snack 12
 // Dare la possibilità di inserire due parole.
 
-let word = wordLength('cane','pizza')
+const userwordA =prompt('type a word');
+const userwordB =prompt('type a word');
 
+if(wordLength(userwordA,userwordB)== true){
+    console.log(userwordA,userwordB);
+}else{
+    if(userwordA.length > userwordB.length){
+        console.log(userwordA);
+    }else{
+        console.log(userwordB);
+    }
+}
 //Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. 
 
-function wordLength(wordLengthA,wordLengthB){
-    wordLengthA = 4;
-    wordLengthB = 5;
+function wordLength(wordA,wordB){
+   
 
 
 //Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
-    if(wordLengthA = wordLengthB){
-        return = wordLengthA,wordLengthB;
-    }else if(wordLengthA>wordLengthB){
-        return = wordLengthA;
+    if(wordA.length == wordB.length){
+        return true;
     }else{
-        return = wordLengthB;
-    }
+        return false;
 
 }
-
+}
