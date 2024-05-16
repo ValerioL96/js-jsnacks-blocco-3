5a0e64fcb6a09c63ea3db87234767a812333534e
ValerioL96 <v.lembo1396@gmail.com>
2024-05-16 11:34:52 +0200

    Added function Snack-12

---
+++ Snack-12/js/script.js
@@ -0,0 +1,10 @@
+// Snack 12
+// Dare la possibilità di inserire due parole.
+
+let word = wordLength('cane','pizza')
+
+function wordLength(wordLengthA,wordLengthB){
+    
+}
+//Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. 
+//Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
