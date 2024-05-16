e04b2ff0c2f772d343060aa009a44fef66c03816
ValerioL96 <v.lembo1396@gmail.com>
2024-05-16 11:44:33 +0200

    Snack-12

--- Snack-12/js/script.js
+++ Snack-12/js/script.js
@@ -3,8 +3,22 @@
 
 let word = wordLength('cane','pizza')
 
-function wordLength(wordLengthA,wordLengthB){
-    
-}
 //Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. 
+
+function wordLength(wordLengthA,wordLengthB){
+    wordLengthA = 4;
+    wordLengthB = 5;
+
+
 //Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+    if(wordLengthA = wordLengthB){
+        return = wordLengthA,wordLengthB;
+    }else if(wordLengthA>wordLengthB){
+        return = wordLengthA;
+    }else{
+        return = wordLengthB;
+    }
+
+}
+
