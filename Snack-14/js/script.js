d0f59cb15ccbbaab268d21f442c7ba6d2f85fb33
ValerioL96 <v.lembo1396@gmail.com>
2024-05-21 12:40:42 +0200

    Added timer

--- Snack-14/js/script.js
+++ Snack-14/js/script.js
@@ -5,3 +5,12 @@ let userNumberSeconds = Number.parseInt(prompt('type to seconds'),10);
 if(Number.isNaN(userNumberSeconds)|| userNumberSeconds> 12){
     userNumberSeconds = 6;
 }
+
+const timerSeconds = setInterval(function(){
+    userNumberSeconds -- ;
+    console.log(userNumberSeconds);
+    if(userNumberSeconds = 0 ){
+        clearInterval(timerSeconds);
+        document.innerHtml(output);
+    }
+})
