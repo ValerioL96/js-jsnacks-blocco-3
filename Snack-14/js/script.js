2eec20816e35dc8f204a9b9fd4061e04c59c0b08
ValerioL96 <v.lembo1396@gmail.com>
2024-05-21 12:31:15 +0200

    Added user number seconds whit the condition

---
+++ Snack-14/js/script.js
@@ -0,0 +1,7 @@
+// ! Chiedi all'utente un numero di secondi da attendere e fai partire un timer da quel momento a zero secondi, interrompendo il timer
+
+let userNumberSeconds = Number.parseInt(prompt('type to seconds'),10);
+
+if(Number.isNaN(userNumberSeconds)|| userNumberSeconds> 12){
+    userNumberSeconds = 6;
+}
