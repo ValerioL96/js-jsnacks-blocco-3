424397f0d55605e612b31f6fed984bf80626bf84
ValerioL96 <v.lembo1396@gmail.com>
2024-05-22 12:40:53 +0200

    Added objet palla

---
+++ Snack-15/js/script.js
@@ -0,0 +1,16 @@
+//Creare un oggetto palla che abbia le seguenti proprietà.
+//Nome = palla
+//Peso = 10
+
+let palla = [
+    {
+        nome: 'palla',
+        peso: 10
+    }
+
+   
+]
+
+console.log(palla)
+
+//Attraverso un prompt dare la possibilità all'utente di modificare il peso della palla.
