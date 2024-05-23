e3840ca6129bc47041642e12e984adbb30dca65c
ValerioL96 <v.lembo1396@gmail.com>
2024-05-23 12:10:29 +0200

    Created array cars

---
+++ js- objet-snack-2/js/script.js
@@ -0,0 +1,83 @@
+// > Crea un array composto da 15 automobili.
+// > Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+
+const auto = [
+    {
+        marca:'ford',
+        modello:'fiesta',
+        alimentazione: 'benzina'
+    },
+    {
+        marca:'fiat',
+        modello:'x',
+        alimentazione:'gpl'
+    },
+    {
+        marca:'ford',
+        modello:'puma',
+        alimentazione:'benzina'
+    },
+    {
+        marca:'tesla',
+        modello:'xs',
+        alimentazione:'elettrico'
+    },
+    {
+        marca:'fiat',
+        modello:'panda',
+        alimentazione:'metano'
+    },
+    {
+        marca:'ford',
+        modello:'fiesta',
+        alimentazione: 'benzina'
+    },
+    {
+        marca:'fiat',
+        modello:'x',
+        alimentazione:'gpl'
+    },
+    {
+        marca:'ford',
+        modello:'puma',
+        alimentazione:'benzina'
+    },
+    {
+        marca:'tesla',
+        modello:'xs',
+        alimentazione:'elettrico'
+    },
+    {
+        marca:'fiat',
+        modello:'panda',
+        alimentazione:'metano'
+    },
+    {
+        marca:'ford',
+        modello:'fiesta',
+        alimentazione: 'benzina'
+    },
+    {
+        marca:'fiat',
+        modello:'x',
+        alimentazione:'gpl'
+    },
+    {
+        marca:'ford',
+        modello:'puma',
+        alimentazione:'benzina'
+    },
+    {
+        marca:'tesla',
+        modello:'xs',
+        alimentazione:'elettrico'
+    },
+    {
+        marca:'fiat',
+        modello:'panda',
+        alimentazione:'metano'
+    }
+    
+]
+// < Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+// < Infine stampa separatamente i 3 array.
