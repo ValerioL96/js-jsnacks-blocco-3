1d1c7a7164c52052c10f75aa03024f61302474c2
ValerioL96 <v.lembo1396@gmail.com>
2024-05-23 12:39:13 +0200

    Created list people

---
+++ js-objet-snack-3/script.js
@@ -0,0 +1,58 @@
+
+// Crea un array di oggetti che rappresentano delle persone, 10.
+// Ogni persona ha un nome, un cognome e un'età.
+
+const persone = [
+    {
+        nome ='Francesco',
+        cognome = 'filiberto',
+        età = '18';
+    },
+    {
+        nome ='Francesca',
+        cognome = 'fili',
+        età = '30';
+    },
+    {
+        nome ='Anna',
+        cognome = 'berto',
+        età = '5';
+    },
+    {
+        nome ='Gino',
+        cognome = 'Pasticcino',
+        età = '75';
+    },
+    {
+        nome ='Luis',
+        cognome = 'Cava',
+        età = '50';
+    },
+    {
+        nome ='Francesco',
+        cognome = 'filiberto',
+        età = '18';
+    },
+    {
+        nome ='Francesca',
+        cognome = 'fili',
+        età = '30';
+    },
+    {
+        nome ='Anna',
+        cognome = 'berto',
+        età = '5';
+    },
+    {
+        nome ='Gino',
+        cognome = 'Pasticcino',
+        età = '75';
+    },
+    {
+        nome ='Luis',
+        cognome = 'Cava',
+        età = '50';
+    }
+];
+
+// Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
