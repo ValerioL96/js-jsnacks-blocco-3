b81900c5252c7535da7ce36a86bdae09f52c97bc
ValerioL96 <v.lembo1396@gmail.com>
2024-05-23 12:50:53 +0200

    Added two array for age of drive

--- js-objet-snack-3/script.js
+++ js-objet-snack-3/script.js
@@ -4,55 +4,72 @@
 
 const persone = [
     {
-        nome ='Francesco',
-        cognome = 'filiberto',
-        età = '18';
+        nome :'Francesco',
+        cognome : 'filiberto',
+        età :18
     },
     {
-        nome ='Francesca',
-        cognome = 'fili',
-        età = '30';
+        nome :'Francesca',
+        cognome : 'fili',
+        età :30
     },
     {
-        nome ='Anna',
-        cognome = 'berto',
-        età = '5';
+        nome :'Anna',
+        cognome : 'berto',
+        età: 5
     },
     {
-        nome ='Gino',
-        cognome = 'Pasticcino',
-        età = '75';
+        nome :'Gino',
+        cognome : 'Pasticcino',
+        età:75
     },
     {
-        nome ='Luis',
-        cognome = 'Cava',
-        età = '50';
+        nome :'Luis',
+        cognome : 'Cava',
+        età:50
     },
     {
-        nome ='Francesco',
-        cognome = 'filiberto',
-        età = '18';
+        nome :'Francesco',
+        cognome : 'filiberto',
+        età :18
     },
     {
-        nome ='Francesca',
-        cognome = 'fili',
-        età = '30';
+        nome :'Francesca',
+        cognome : 'fili',
+        età :30
     },
     {
-        nome ='Anna',
-        cognome = 'berto',
-        età = '5';
+        nome :'Anna',
+        cognome : 'berto',
+        età: 5
     },
     {
-        nome ='Gino',
-        cognome = 'Pasticcino',
-        età = '75';
+        nome :'Gino',
+        cognome : 'Pasticcino',
+        età:75
     },
     {
-        nome ='Luis',
-        cognome = 'Cava',
-        età = '50';
+        nome :'Luis',
+        cognome : 'Cava',
+        età:50
     }
 ];
 
+console.log(persone);
+
 // Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
+
+const yesDriver =[];
+const noDriver = [];
+
+array.forEach(element => {
+    if(element.età<18 || element.età>50){
+        noDriver.push(element);
+    }
+    else{ yesDriver.push(element);
+
+    }
+});
+
+console.log(yesDriver);
+console.log(noDriver);
