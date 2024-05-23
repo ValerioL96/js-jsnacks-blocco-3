d080643c672b5740378b98d2ee9ff469cccf3947
ValerioL96 <v.lembo1396@gmail.com>
2024-05-23 12:19:55 +0200

    created three array for alimentazione

--- js- objet-snack-2/js/script.js
+++ js- objet-snack-2/js/script.js
@@ -1,6 +1,6 @@
 // > Crea un array composto da 15 automobili.
 // > Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
-
+let alimentazione;
 const auto = [
     {
         marca:'ford',
@@ -79,5 +79,22 @@ const auto = [
     }
     
 ]
+
 // < Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+const autoBenzina= [];
+const auotDiesel = [];
+const auotoDIverseAlim=[];
+
+array.forEach(element => {
+    if(alimentazione= 'benzina'){
+        auto.push(autoBenzina);
+    }else if(alimentazione = 'diesel'){
+        auto.push(auotDiesel);
+    }else{
+        auto.push(auotoDIverseAlim)
+    }
+   console.log(autoBenzina,auotDiesel,auotoDIverseAlim)
+});
+
+
 // < Infine stampa separatamente i 3 array.
